27b090099c223a813aea26f5cfcd704e58f9a610
Kai Wu Toh <toh_kai_wu@tech.gov.sg>
2020-09-01 17:12:03 +0800

    feat: added lightbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Nav from "./components/Nav/Nav";
 import Content from "./components/Content/Content.jsx";
 import { Grid } from "@material-ui/core";
@@ -7,47 +7,51 @@ import FacebookIcon from "@material-ui/icons/Facebook";
 import InstagramIcon from "@material-ui/icons/Instagram";
 import GitHubIcon from "@material-ui/icons/GitHub";
 import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import SimpleReactLightbox from "simple-react-lightbox";
+
 import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Grid container alignItems="stretch" className="App">
-        <Grid item xs={12} sm={3}>
-          <div className="side-bar">
-            <Nav />
-            <div className="description">
-              <h1 className="welcome-message">Hello </h1>
-              <p className="description-message">
-                I am Kai Wu. A developer that tries to convert chocolate to code
-                and an aspiring photographer. Check out my projects and
-                creations :)
-              </p>
-              <div className="social-media">
-                <Grid container justify="space-between" alignItems="center">
-                  <a href="https://www.facebook.com/kaiwu.toh">
-                    <FacebookIcon className="social-media-icons" />
-                  </a>
-                  <a href="https://www.instagram.com/kaiwuuu">
-                    <InstagramIcon className="social-media-icons" />
-                  </a>
-                  <a href="https://github.com/kwtoh">
-                    <GitHubIcon className="social-media-icons" />
-                  </a>
-                  <a href="https://www.linkedin.com/in/kai-wu-toh/">
-                    <LinkedInIcon className="social-media-icons" />
-                  </a>
-                </Grid>
+    <SimpleReactLightbox>
+      <Router>
+        <Grid container alignItems="stretch" className="App">
+          <Grid item xs={12} sm={3}>
+            <div className="side-bar">
+              <Nav />
+              <div className="description">
+                <h1 className="welcome-message">Hello </h1>
+                <p className="description-message">
+                  I am Kai Wu. A developer that tries to convert chocolate to
+                  code and an aspiring photographer. Check out my projects and
+                  creations :)
+                </p>
+                <div className="social-media">
+                  <Grid container justify="space-between" alignItems="center">
+                    <a href="https://www.facebook.com/kaiwu.toh">
+                      <FacebookIcon className="social-media-icons" />
+                    </a>
+                    <a href="https://www.instagram.com/kaiwuuu">
+                      <InstagramIcon className="social-media-icons" />
+                    </a>
+                    <a href="https://github.com/kwtoh">
+                      <GitHubIcon className="social-media-icons" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/kai-wu-toh/">
+                      <LinkedInIcon className="social-media-icons" />
+                    </a>
+                  </Grid>
+                </div>
               </div>
+              <footer>Copyright © 2020 Kai Wu. All rights reserved.</footer>
             </div>
-            <footer>Copyright © 2020 Kai Wu. All rights reserved.</footer>
-          </div>
+          </Grid>
+          <Grid item xs={12} sm={9}>
+            <Content />
+          </Grid>
         </Grid>
-        <Grid item xs={12} sm={9}>
-          <Content />
-        </Grid>
-      </Grid>
-    </BrowserRouter>
+      </Router>
+    </SimpleReactLightbox>
   );
 }
 
--- src/components/Creations/Creations.jsx
+++ src/components/Creations/Creations.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Masonry from "react-masonry-css";
 import { PhotoCard } from "../PhotoCard";
 import "./Creation.css";
+import { SRLWrapper } from "simple-react-lightbox";
 
 const breakpointColumnsObj = {
   default: 5,
@@ -10,19 +11,22 @@ const breakpointColumnsObj = {
 };
 
 const Creations = ({ images }) => {
-  const imageComponent = images.map(
-    ({ image, altText }) => <PhotoCard image={image} altText={altText} />
-  );
+  const imageComponent = images.map(({ image, altText }) => (
+    <PhotoCard image={image} altText={altText} />
+  ));
 
   return (
     <>
       <div className="photos-grid">
-        <Masonry
-          breakpointCols={breakpointColumnsObj}
-          className="photos"
-          columnClassName="photos_column">
-          {imageComponent}
-        </Masonry>
+        <SRLWrapper>
+          <Masonry
+            breakpointCols={breakpointColumnsObj}
+            className="photos"
+            columnClassName="photos_column"
+          >
+            {imageComponent}
+          </Masonry>
+        </SRLWrapper>
       </div>
     </>
   );
